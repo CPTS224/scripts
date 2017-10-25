e3ae8fe01fe44ffc364ffebe119b095c10fe7f86
Henry <michael.j.henry@elec.tricity.wsu.edu>
2017-10-24 17:13:55 -0700

    make file stuff

---
+++ advanced_make_ex/example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "other.h"
+
+int main(void){
+
+	printf("Hello, CPTS 224\n\n");
+
+	int retVal = other(3);
+
+	printf("The value returned is %i\n\n",retVal);
+
+	return 0;
+}
---
+++ advanced_make_ex/other.c
@@ -0,0 +1,5 @@
+#include "other.h"
+
+int other(int x) {
+	return x * 2;
+}
